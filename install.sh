b2791bafc1e049b909b0d0ed7ae0cb3aa7b8c9da
mrx04programmer <mjuandavid901@gmail.com>
2022-09-24 11:29:29 -0500

    Colors in termux zoo

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ blueColour="\e[0;34m\033[1m"
 yellowColour="\e[0;33m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
-echo -e "${greenColour}[+] Instalando dependencia w3m...${endColour}"
+echo "${greenColour}[+] Instalando dependencia w3m...${endColour}"
 apt update && apt install w3m -y
-echo -e "${greenColour}[+] Instalando dependencia nmap...${endColour}"
+echo "${greenColour}[+] Instalando dependencia nmap...${endColour}"
 apt install nmap -y
-echo -e "${greenColour}[+] Instalando dependencia wget...${endColour}"
+echo "${greenColour}[+] Instalando dependencia wget...${endColour}"
 python3 -m pip install wget
-echo -e "${yellowColour}[o] Creando ejecutable de termux-zoo...${endColour}"
+echo "${yellowColour}[o] Creando ejecutable de termux-zoo...${endColour}"
 cp ./termux-zoo.py $PREFIX/bin/termux-zoo
 chmod 777 $PREFIX/bin/termux-zoo
 chmod 777 ./.s
-echo -e "${greenColour}[x] Instalación terminada exitosamente."
-echo -e "${greenColour}[x] Ejecuta el comando : ${grayColour}termux-zoo <modulo> <host/ip>${endColour}"
+echo "${greenColour}[x] Instalación terminada"
+echo "${greenColour}[x] Ejecuta el comando : ${grayColour}termux-zoo <modulo> <host/ip>${endColour}"
